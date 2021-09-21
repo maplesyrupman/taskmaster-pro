2dd21b938b0274855f8364a408f525bdf7723c04
maplesyrupman <maplesyrupman@protonmail.com>
2021-09-21 12:53:38 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,7 +112,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -149,16 +149,18 @@ $('.card .list-group').sortable({
   tolerance: 'pointer',
   helper: 'clone', 
   activate: function(event) {
-
+    $(this).addClass('dropover')
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
-
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event) {
-
+    $(this).addClass('dropover-active');
   },
   out: function(event) {
-
+    $(this).removeClass('dropover-active');
   },
   update: function(event) {
     let tempArr = [];
@@ -174,6 +176,7 @@ $('.card .list-group').sortable({
     let arrName = $(this).attr('id').replace('list-', '');
     tasks[arrName] = tempArr;
     saveTasks();
+    $(this).removeClass('dropover');
   }
 });
 
@@ -183,12 +186,13 @@ $('#trash').droppable({
   tolerance: 'touch', 
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(this).removeClass('bottom-trash-active');
   },
   over: function(event, ui) {
-    console.log('over');
+    $(this).addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    console.log('out');
+    $(this).removeClass('bottom-trash-active');
   }
 });
 
@@ -214,4 +218,9 @@ let auditTask = (taskEl) => {
 // load tasks for the first time
 loadTasks();
 
+setInterval(function() {
+  $('.card .list-group-item').each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30 );
 
